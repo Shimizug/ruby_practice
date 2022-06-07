e938bd64332a953aee475aa5aabb2415c4ec54b6
EC2 Default User <ec2-user@ip-172-31-35-94.ap-northeast-1.compute.internal>
2022-06-07 14:19:59 +0000

    [add]if

--- pra.rb
+++ pra.rb
@@ -1,2 +1,12 @@
 subjects = ["国語", "算数", "理科", "社会"]
 puts subjects[2]
+
+total_price = 100
+
+if total_price > 100
+  puts "みかんを購入、所持金にあまりあり"
+elsif total_price == 100
+  puts "みかんを購入、所持金0円"
+else
+  "足りません"
+end
